b01f1c1805ffe7693e4f102b0cd68ee919f726a9
laramackey <lara.mackey@focusrite.com>
2020-11-26 11:26:03 +0000

    add bookings table locally

---
+++ back-end/db/migrations/002-add-dummy-bookings.sql
@@ -0,0 +1,8 @@
+  
+INSERT INTO bookings VALUES (1, '2020-11-26', 'Lara Mackey', 1);
+INSERT INTO bookings VALUES (1, '2020-11-27', 'Lara Mackey', 1);
+INSERT INTO bookings VALUES (1, '2020-11-28', 'Jack Nolan', 1);
+INSERT INTO bookings VALUES (2, '2020-11-26', 'Will Charlton', 1);
+INSERT INTO bookings VALUES (3, '2020-11-26', 'Jack Nolan', 1);
+INSERT INTO bookings VALUES (3, '2020-11-27', 'Glenn Barr', 1);
+INSERT INTO bookings VALUES (4, '2020-11-26', 'Rhea Ramtohul', 1);
