72860706d4066c3a5f8cbaf969fa56c1f58628bc
laramackey <lara.mackey@focusrite.com>
2020-11-26 12:27:23 +0000

    Return bookings from db without date filter

--- back-end/src/lib/app.ts
+++ back-end/src/lib/app.ts
@@ -3,19 +3,17 @@ import * as bodyParser from 'koa-body-parser';
 const app = new Koa();
 import * as Router from '@koa/router';
 const router = new Router();
+import {addDb} from './db';
+import getHandler from './get-bookings';
 
-router.get('/bookings', (ctx) => {
-  ctx.body = {
-    bookings: [],
-  };
-  ctx.status = 200;
-});
+router.get('/bookings', getHandler);
 
 export default () => {
   app.use(bodyParser());
   app.use(async (ctx, next) => {
     return await next();
   });
+  app.use(addDb());
   app.use(router.routes()).use(router.allowedMethods());
   return app;
 };
